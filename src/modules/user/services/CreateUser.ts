95ea163ac2f017d28021790877d5984f73fae621
Felipe Suknaic <fsuknaic@hotmail.com>
2021-03-11 15:01:42 -0500

    service de creacao de usuario

--- src/modules/user/services/CreateUser.ts
+++ src/modules/user/services/CreateUser.ts
@@ -1,3 +1,23 @@
+import { IUserRepository } from "@modules/user/repositories/IUserRepository";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/user";
+
 export class CreateUser {
-  async execulte() {}
+  constructor(private userRepository: IUserRepository) {}
+
+  async execulte(data: ICreateUserDTO) {
+    const userAlredyExists = await this.userRepository.fyndByEmail(data.email);
+
+    if (userAlredyExists) {
+      throw new Error("User alredy exists");
+    }
+
+    const user = new User(data);
+
+    await this.userRepository.save(user);
+  }
 }
+
+// services nao tem ideia de qual tipo de ORM/Database vai ser usado
+// ele faz uso de uma interface que contem os metodos para ser usado no servico
+//[D]ependency inversion principle
