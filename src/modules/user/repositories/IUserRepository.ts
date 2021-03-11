97296ff9a235a0da30106b87ae01bf10adc90ede
Felipe Suknaic <fsuknaic@hotmail.com>
2021-03-11 15:36:32 -0500

    anotacoes do repositorio

--- src/modules/user/repositories/IUserRepository.ts
+++ src/modules/user/repositories/IUserRepository.ts
@@ -4,3 +4,7 @@ export interface IUserRepository {
   fyndByEmail(email: string): Promise<User>;
   save(user: User): Promise<void>;
 }
+
+// interface userRepository
+// e um contrato que contem os methodos
+// que devem ser usadados para salvar um usuario
